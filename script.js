564dc338d3597bfcdb8e2d84c8db7ae85b52ed6b
Jaws-coder <Sheharyar.adnan08@gmail.com>
2024-11-10 13:54:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.getElementById("signin").addEventListener("click", function () {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-  if (username && password) {
-    // Handle the form submission here
-    console.log("Username:", username);
-    console.log("Password:", password);
-  } else {
-    alert("Please fill in both fields.");
-  }
-});
 
