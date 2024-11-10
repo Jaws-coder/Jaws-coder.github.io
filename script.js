a47979417f1f6754bbc8feb1a87bbdccd245c8f3
Jaws-coder <Sheharyar.adnan08@gmail.com>
2024-11-10 14:46:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let currentImageIndex = 0;
+const images = [
+  "https://us.rule34.xxx/thumbnails/2366/thumbnail_272c0c593582aa9b41ee21645d68f1bf.jpg?10415767",
+  "https://us.rule34.xxx//samples/1806/sample_5a8e0f8fbb26c4ca5f59076f00391d45.jpg?9901801",
+  "https://us.rule34.xxx//samples/2257/sample_90a9dff83c8d06990bcb98cdaa467104.jpg?11132016",
+  "https://us.rule34.xxx//images/5503/6ca0e7a3f02f8c1deaef43c398fc7d9a.jpeg?10788806",
+  "https://us.rule34.xxx//images/2366/ff7e51748c333b1b41ef713ab01d02dd.jpeg?10415710",
+  "https://us
 
